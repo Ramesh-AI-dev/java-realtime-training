e6a860b27c3a8823e76086579c4998eef2f13d06
ramesh <ramesh.mygithub@gmail.com>
2026-02-17 12:17:08 -0600

    first java training project

---
+++ src/Day1Practice.java
@@ -0,0 +1,14 @@
+public class Day1Practice {
+
+    public static void main(String[] args) {
+
+       String city="EDEN PRAIRIE";
+       int age = 26;
+       double coursefee = 499.99;
+
+       System.out.println("city:"+city);
+        System.out.println("age:"+age);
+        System.out.println("fee:"+coursefee);
+
+    }
+}
---
+++ src/StudentSystem.java
@@ -0,0 +1,34 @@
+public class StudentSystem {
+    public static void main(String[] args){
+
+        String student1 ="Abhi";
+        int student1_id = 123;
+        double fee1 = 1299.99;
+
+        String student2 = "Bharath";
+        int student2_id =124;
+        double fee2 = 1299.99;
+
+        String student3 ="Raja";
+        int student3_id= 125;
+        double fee3=1299.99;
+
+        System.out.println("student1:"+student1);
+        System.out.println("Student1 id:"+student1_id);
+        System.out.println("fee1"+fee1);
+
+        System.out.println("-----------------");
+
+
+        System.out.println("student1:"+student1);
+        System.out.println("Student1 id:"+student1_id);
+        System.out.println("fee1"+fee1);
+
+        System.out.println("-----------------");
+
+
+        System.out.println("student1:"+student1);
+        System.out.println("Student1 id:"+student1_id);
+        System.out.println("fee1"+fee1);
+    }
+}
